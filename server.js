e15b1f1fada124e3aa90808ca6380f3c8869297e
dilip ahir <ahirdilp@gmail.com>
2022-09-24 00:02:15 +0530

    changes

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const mongoose = require("mongoose");
 const todoRouter = require("./src/routes/todo/todo.routes");
 
 const PORT = process.env.PORT || 8000;
-const MONGO_URL = process.env.MONGODB_URI || "mongodb+srv://dilip:zyu7iaN3YtASjaLE@cluster0.2fwvxj3.mongodb.net/?retryWrites=true&w=majority";
+
+const MONGO_URL = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/test";
 
 const app = express();
 
@@ -24,13 +25,11 @@ mongoose.connection.on("error", (err) => {
   console.error(err);
 });
 
-//if(process.env.NODE_ENV=='production'){
      app.use(express.static('client/build'));
      app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-//}
 
 async function main() {
   await mongoose.connect(MONGO_URL, {
